094a3d3d6ed396e08e55ea972e0aceba74d0fc8d
brobat6 <ldrubra8@gmail.com>
2023-04-09 15:34:01 +0530

    Slightly improved sample implementation

--- type_checking_sample.c
+++ type_checking_sample.c
@@ -2,14 +2,18 @@
 
 int TypeCheckingDFS(Ast_Node* root) {
 
-
-    if(root->type == 35 && root->child_2 != NULL) {
-        // Means we are at N7 now.
-        int right_type = TypeCheckingDFS(root->syn_next);
-        int left_type = TypeCheckingDFS(root->inh_1);
-        if(right_type != left_type) {
-            // THROW AN ERROR.
+    if(root->type == 35) {
+        if(root->child_2 == NULL) {
+            // we are at arithmeticOrBooleanExpression
+            int type_of_expression = TypeCheckingDFS(root->syn_next);
+            return type_of_expression;
+        } else {
+            int right_type = TypeCheckingDFS(root->syn_next);
+            int left_type = TypeCheckingDFS(root->inh_1);
+            if(right_type != left_type) {
+                // THROW AN ERROR.
+            }
+            return left_type;
         }
-        return left_type;
     }
 }
