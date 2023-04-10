d0746f0413f772854107c5b4bf0f689534b83405
brobat6 <ldrubra8@gmail.com>
2023-04-10 18:43:15 +0530

    Added function definitions

--- symbolTable.h
+++ symbolTable.h
@@ -4,19 +4,22 @@
 #define _SYMBOLTABLE_H
 
 STTreeNode* createSTTreeNode();
-
 void addSTChild(STTreeNode *parent,STTreeNode *child);
-
 STEntry* createSTEntry(STEntry* prevEntry);
-
 STEntry* checkID(STTreeNode* node,char lexeme[]);
-
+FunctionSTEntry* checkFunctionID(char lexeme[]);
 STEntry* recursiveCheckID(STTreeNode* node,Token_Info* t);
-
-void generateST(STTreeNode* currSTNode, Ast_Node* root, Ast_Node* prev);
-
+ParamList* initialize_parameter_list();
+void add_to_parameter_list(ParamList* pl, STEntry* st);
+Type get_entry_type(enum Token tokenType);
+int get_entry_width(Type type);
+void populate_temporary_function_entry(Ast_Node* temp);
+void throw_function_already_exists_error(char module_name[], int line_no);
+void throw_already_exists_error(char module_name[], int line_no);
+void throw_function_not_declared_error(char module_name[], int line_no);
+void generateST(STTreeNode* currSTNode, Ast_Node* root);
 STTreeNode* generateSymbolTable(Ast_Node* ASTRoot);
-
-void printSymbolTable();
+void recursive_print_symbol_table(STTreeNode* root, FILE* fp);
+void print_symbol_table(STTreeNode* root, FILE* fp);
 
 #endif
