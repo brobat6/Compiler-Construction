2dac2619fff610fcae5943998066f483f0e5ae6e
Shivam Pande <74484164+shiworm@users.noreply.github.com>
2023-04-12 05:11:41 +0530

    umfinal

--- typeCheck.h
+++ typeCheck.h
@@ -3,7 +3,7 @@
 #ifndef _TC_H
 #define _TC_H
 
-Type checkType(Operator op, Type t1, Type t2);
+Type checkType(Token_Info* tk_data, Operator op, Type t1, Type t2);
 
 Operator token_to_op(Token t);
 
